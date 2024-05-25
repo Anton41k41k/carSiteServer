506a45ceab73274d6b737c373248a55dbdff9a5f
Anton <you@example.com>
2024-05-26 02:54:00 +0300

    add: new router

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import dotenv from 'dotenv';
 import Feedback from '../db/models/Feedback.js';
+import Order from '../db/models/Order.js';
 
 dotenv.config();
 export const router = Router();
 router.post('/feedback', async (req, res) => {
   const feedback = new Feedback(req.body);
   feedback.save();
-  console.log('Сервер ответил');
-  // console.log();
+  console.log('Сервер отправил feedback');
+});
+router.post('/order', async (req, res) => {
+  const order = new Order(req.body);
+  order.save();
+  console.log('Сервер отправил order');
 });
